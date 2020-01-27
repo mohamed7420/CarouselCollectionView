0d0161776560a115c33b1df74aa832f956c29e72
mohamedosama2074 <iosengks74@gmail.com>
2020-01-27 12:59:39 +0200

    Complete the CollectionView

---
+++ CarouselCollectionView/CustomCollectionCell.swift
@@ -0,0 +1,19 @@
+//
+//  CustomCollectionCell.swift
+//  CarouselCollectionView
+//
+//  Created by Mohamed on 1/27/20.
+//  Copyright © 2020 Mohamed74. All rights reserved.
+//
+
+import UIKit
+
+class CustomCollectionCell: UICollectionViewCell{
+    
+    @IBOutlet weak var collectionView: UIView!
+    @IBOutlet weak var collectionLabel: UILabel!
+    @IBOutlet weak var imageCollection: UIImageView!
+    
+    
+    
+}
--- CarouselCollectionView/ViewController.swift
+++ CarouselCollectionView/ViewController.swift
@@ -9,12 +9,50 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var interestLabel: UILabel!
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var arrayOfImages:[UIImage] = [UIImage(named: "images1")! , UIImage(named: "image2")! , UIImage(named: "image3")!]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+
     }
 
 
 }
 
+extension ViewController: UICollectionViewDelegate , UICollectionViewDataSource{
+    
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionCell", for: indexPath) as! CustomCollectionCell
+        
+        let image = arrayOfImages[indexPath.row]
+        
+        cell.imageCollection.image = image
+        
+        return cell
+    }
+    
+    
+    
+    
+}
