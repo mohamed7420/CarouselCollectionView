247ac7c8ea79382104046f02fbcd4370155dd825
mohamedosama2074 <iosengks74@gmail.com>
2020-01-27 12:01:30 +0200

    Initial Commit

---
+++ CarouselCollectionView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CarouselCollectionView
+//
+//  Created by Mohamed on 1/27/20.
+//  Copyright © 2020 Mohamed74. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
